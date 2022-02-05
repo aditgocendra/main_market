caa89ee666f8562092a1386f61220be0840588bb
aditgocendra <gocendra123@gmail.com>
2022-02-05 12:13:02 +0800

    add function updateUI

--- app/src/main/java/com/ark/mainmarket/MainActivity.java
+++ app/src/main/java/com/ark/mainmarket/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.ark.mainmarket.View.User.HomeApp;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Utility.updateUI(MainActivity.this, HomeApp.class);
     }
 }
--- app/src/main/java/com/ark/mainmarket/Utility.java
+++ app/src/main/java/com/ark/mainmarket/Utility.java
@@ -1,4 +1,12 @@
 package com.ark.mainmarket;
 
+import android.content.Context;
+import android.content.Intent;
+
 public class Utility {
+
+    public static void updateUI(Context from, Class to){
+        Intent intent = new Intent(from, to);
+        from.startActivity(intent);
+    }
 }
