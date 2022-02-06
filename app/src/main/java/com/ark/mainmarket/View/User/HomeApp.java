b19cd63e54938e792f0dc32ff8db83744b74b3ba
aditgocendra <gocendra123@gmail.com>
2022-02-06 22:13:31 +0800

    add fragment home app activity

--- app/src/main/java/com/ark/mainmarket/View/User/HomeApp.java
+++ app/src/main/java/com/ark/mainmarket/View/User/HomeApp.java
@@ -1,6 +1,9 @@
 package com.ark.mainmarket.View.User;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.annotation.SuppressLint;
 import android.graphics.Color;
@@ -10,13 +13,14 @@ import android.view.Window;
 import android.view.WindowManager;
 
 import com.ark.mainmarket.R;
+import com.ark.mainmarket.View.Fragment.Favorite;
+import com.ark.mainmarket.View.Fragment.Feed;
+import com.ark.mainmarket.View.Fragment.Home;
 import com.ark.mainmarket.databinding.ActivityHomeAppBinding;
 
 public class HomeApp extends AppCompatActivity {
 
     private ActivityHomeAppBinding binding;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,29 +28,32 @@ public class HomeApp extends AppCompatActivity {
         binding = ActivityHomeAppBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-//        Window window = this.getWindow();
-          getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
 
-//        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-//        window.setStatusBarColor(this.getResources().getColor(R.color.blue_cyan));
+        changeFragment(new Home());
+        binding.bottomNavbar.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.home:
+                    changeFragment(new Home());
+                    break;
+                case R.id.feed:
+                    changeFragment(new Feed());
+                    break;
+                case R.id.fav:
+                    changeFragment(new Favorite());
+                    break;
+            }
 
-
-
-        setSupportActionBar(binding.toolbar);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
-
-
-
-
-
+            return true;
+        });
 
     }
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        super.onCreateOptionsMenu(menu);
-//
-//        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
-//        return true;
-//    }
+    private void changeFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.setCustomAnimations(R.anim.nav_default_pop_enter_anim, R.anim.nav_default_pop_exit_anim);
+        fragmentTransaction.replace(R.id.frame_layout_fragment, fragment);
+        fragmentTransaction.commit();
+    }
 }
