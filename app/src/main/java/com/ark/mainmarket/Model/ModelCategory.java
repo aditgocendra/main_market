573fe92d1cd2391ba0f75e1f16cc632cd11ca6d4
aditgocendra <gocendra123@gmail.com>
2022-02-09 16:58:37 +0800

    add model category

---
+++ app/src/main/java/com/ark/mainmarket/Model/ModelCategory.java
@@ -0,0 +1,41 @@
+package com.ark.mainmarket.Model;
+
+public class ModelCategory {
+
+    private String name_category;
+    private String icon_link;
+    private String key;
+
+    public ModelCategory(){
+
+    }
+
+    public ModelCategory(String name_category, String icon_link) {
+        this.name_category = name_category;
+        this.icon_link = icon_link;
+    }
+
+    public String getName_category() {
+        return name_category;
+    }
+
+    public void setName_category(String name_category) {
+        this.name_category = name_category;
+    }
+
+    public String getIcon_link() {
+        return icon_link;
+    }
+
+    public void setIcon_link(String icon_link) {
+        this.icon_link = icon_link;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+}
