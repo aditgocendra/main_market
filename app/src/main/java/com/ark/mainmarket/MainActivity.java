11dfb590f76a8251fd6e606fe5c484175acc6518
aditgocendra <gocendra123@gmail.com>
2022-02-10 17:07:02 +0800

    add splash screen

--- app/src/main/java/com/ark/mainmarket/MainActivity.java
+++ app/src/main/java/com/ark/mainmarket/MainActivity.java
@@ -3,10 +3,13 @@ package com.ark.mainmarket;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.WindowManager;
 
 import com.ark.mainmarket.View.Admin.AddCategory;
 import com.ark.mainmarket.View.Admin.AddProduct;
+import com.ark.mainmarket.View.Auth.Login;
 import com.ark.mainmarket.View.User.HomeApp;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,8 +18,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Utility.updateUI(MainActivity.this, Login.class);
+                finish();
+            }
+        }, 1000);
 
 
-        Utility.updateUI(MainActivity.this, AddProduct.class);
     }
 }
