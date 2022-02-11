61a3a31de1843d9279c42533703a90b2a04e4758
aditgocendra <gocendra123@gmail.com>
2022-02-11 21:44:45 +0800

    add condition user login

--- app/src/main/java/com/ark/mainmarket/MainActivity.java
+++ app/src/main/java/com/ark/mainmarket/MainActivity.java
@@ -10,7 +10,10 @@ import android.view.WindowManager;
 import com.ark.mainmarket.View.Admin.AddCategory;
 import com.ark.mainmarket.View.Admin.AddProduct;
 import com.ark.mainmarket.View.Auth.Login;
+import com.ark.mainmarket.View.Fragment.Home;
 import com.ark.mainmarket.View.User.HomeApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,7 +27,14 @@ public class MainActivity extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Utility.updateUI(MainActivity.this, Login.class);
+                FirebaseUser mUser = FirebaseAuth.getInstance().getCurrentUser();
+                if (mUser != null){
+                    Utility.updateUI(MainActivity.this, HomeApp.class);
+                    Utility.uidCurrentUser = mUser.getUid();
+                }else {
+                    Utility.updateUI(MainActivity.this, Login.class);
+                }
+
                 finish();
             }
         }, 1000);
