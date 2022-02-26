56b82b5879c8eb2413fb91ae4f446418953103e6
aditgocendra <gocendra123@gmail.com>
2022-02-26 13:30:45 +0800

    add change preview name in realtime

--- app/src/main/java/com/ark/mainmarket/View/Admin/AddCategory.java
+++ app/src/main/java/com/ark/mainmarket/View/Admin/AddCategory.java
@@ -10,28 +10,26 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
+
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
-import android.view.WindowManager;
-import android.widget.Toast;
+
 
 import com.ark.mainmarket.Model.ModelCategory;
-import com.ark.mainmarket.Model.ModelProduct;
+
 import com.ark.mainmarket.Utility;
 import com.ark.mainmarket.databinding.ActivityAddCategoryBinding;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
+
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
-import java.io.ByteArrayOutputStream;
+
 import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -52,12 +50,16 @@ public class AddCategory extends AppCompatActivity {
         setContentView(binding.getRoot());
         Utility.login();
 
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        Utility.checkWindowSetFlag(this);
         listenerClick();
 
     }
 
     private void listenerClick(){
+        binding.backBtn.setOnClickListener(view -> {
+            finish();
+        });
+
         binding.selectIconBtn.setOnClickListener(view -> {
             if (ActivityCompat.checkSelfPermission(AddCategory.this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
                 pickImageOnGalery();
@@ -67,24 +69,39 @@ public class AddCategory extends AppCompatActivity {
             }
         });
 
-        binding.previewIconBtn.setOnClickListener(view -> {
-            if (binding.nameCategoryAdd.getText().toString().isEmpty()){
-                binding.nameCategoryAdd.setError("Nama kategori kosong");
-            }else {
-                binding.previewNameCategory.setText(binding.nameCategoryAdd.getText().toString());
-            }
-        });
-
         binding.saveBtnCategory.setOnClickListener(view -> {
             if (binding.nameCategoryAdd.getText().toString().isEmpty()){
                 binding.nameCategoryAdd.setError("Nama kategori kosong");
             }else {
                 if (onPickMedia > 0){
                     binding.saveBtnCategory.setEnabled(false);
+                    binding.progressCircular.setVisibility(View.VISIBLE);
                     saveIconCategory();
                 }else {
                     Utility.toastLS(AddCategory.this, "Anda belum mengupload icon");
                 }
+            }
+        });
+
+        binding.nameCategoryAdd.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (charSequence.length() == 0){
+                    binding.previewNameCategory.setText("Nama Kategori");
+                }else {
+                    String nameCat = binding.nameCategoryAdd.getText().toString();
+                    binding.previewNameCategory.setText(nameCat);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
             }
         });
     }
@@ -95,20 +112,16 @@ public class AddCategory extends AppCompatActivity {
         String fileName = dateFormat.format(now);
 
         StorageReference storageRef = FirebaseStorage.getInstance().getReference("icon_categories/"+fileName);
-
-        Bitmap bitmap = ((BitmapDrawable) binding.imageIcon.getDrawable()).getBitmap();
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
-        byte[] data = baos.toByteArray();
-
-        UploadTask uploadTask = storageRef.putBytes(data);
+        UploadTask uploadTask = storageRef.putFile(fileUri);
 
         uploadTask.addOnFailureListener(e -> {
             Utility.toastLS(AddCategory.this, "Icon gagal diupload");
             binding.saveBtnCategory.setEnabled(true);
+            binding.progressCircular.setVisibility(View.GONE);
         }).addOnSuccessListener(taskSnapshot -> storageRef.getDownloadUrl().addOnSuccessListener(uri -> saveCategory(String.valueOf(uri))).addOnFailureListener(e -> {
             Utility.toastLS(AddCategory.this, "Url photo gagal di unduh");
             binding.saveBtnCategory.setEnabled(true);
+            binding.progressCircular.setVisibility(View.GONE);
         }));
     }
 
@@ -125,9 +138,12 @@ public class AddCategory extends AppCompatActivity {
                 Utility.toastLS(AddCategory.this, "Berhasil menambahkan kateg" +
                         "ori");
                 binding.saveBtnCategory.setEnabled(true);
+                binding.progressCircular.setVisibility(View.GONE);
+                finish();
             }else {
                 Utility.toastLS(AddCategory.this, "Gagal menambahkan kategori");
                 binding.saveBtnCategory.setEnabled(true);
+                binding.progressCircular.setVisibility(View.GONE);
             }
         });
     }
