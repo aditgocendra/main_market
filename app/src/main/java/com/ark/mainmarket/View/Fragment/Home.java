816366cadac21b599dca1bd007fb0ac7c6d4a68f
aditgocendra <gocendra123@gmail.com>
2022-02-28 15:27:27 +0800

    get data category and new product from firebase

--- app/src/main/java/com/ark/mainmarket/View/Fragment/Home.java
+++ app/src/main/java/com/ark/mainmarket/View/Fragment/Home.java
@@ -2,19 +2,27 @@ package com.ark.mainmarket.View.Fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
-
-import com.ark.mainmarket.R;
+import com.ark.mainmarket.Adapter.AdapterHomeCategory;
+import com.ark.mainmarket.Adapter.AdapterHomeNewProduct;
+import com.ark.mainmarket.Model.ModelCategory;
+import com.ark.mainmarket.Model.ModelProduct;
 import com.ark.mainmarket.Utility;
-import com.ark.mainmarket.View.Auth.Login;
 import com.ark.mainmarket.View.User.Profile;
 import com.ark.mainmarket.databinding.FragmentHomeBinding;
-import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -57,8 +65,17 @@ public class Home extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setCategoryList();
+        setNewProduct();
     }
+
+    private DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+
+    private List<ModelCategory> listCategory;
+    private List<ModelProduct> listProductNew;
+
+    private AdapterHomeCategory adapterHomeCategory;
+    private AdapterHomeNewProduct adapterHomeNewProduct;
 
     private FragmentHomeBinding binding;
     @Override
@@ -71,9 +88,67 @@ public class Home extends Fragment {
             Utility.updateUI(getActivity(), Profile.class);
         });
 
+        // category recycle
+        RecyclerView.LayoutManager layoutManagerCategory = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
+        binding.recycleCategoryHorizontal.setHasFixedSize(true);
+        binding.recycleCategoryHorizontal.setLayoutManager(layoutManagerCategory);
 
+        // new product recycle
+        RecyclerView.LayoutManager layoutManagerProduct = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
+        binding.recycleProductNewHorizontal.setHasFixedSize(true);
+        binding.recycleProductNewHorizontal.setLayoutManager(layoutManagerProduct);
 
         return binding.getRoot();
 
     }
+
+    private void setCategoryList(){
+        reference.child("category").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                listCategory = new ArrayList<>();
+                for (DataSnapshot ds : snapshot.getChildren()){
+                    ModelCategory modelCategory = ds.getValue(ModelCategory.class);
+                    if (modelCategory != null){
+                        modelCategory.setKey(ds.getKey());
+                        listCategory.add(modelCategory);
+                    }
+                }
+
+                adapterHomeCategory = new AdapterHomeCategory(getContext(), listCategory);
+                binding.recycleCategoryHorizontal.setAdapter(adapterHomeCategory);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Utility.toastLS(getContext(), "Category :"+error.getMessage());
+            }
+        });
+    }
+
+    private void setNewProduct(){
+        reference.child("product").limitToLast(5).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                listProductNew = new ArrayList<>();
+
+                for (DataSnapshot ds : snapshot.getChildren()){
+                    ModelProduct modelProduct = ds.getValue(ModelProduct.class);
+                    if (modelProduct != null){
+                        modelProduct.setKey(ds.getKey());
+                        listProductNew.add(modelProduct);
+                    }
+                }
+                adapterHomeNewProduct = new AdapterHomeNewProduct(getContext(), listProductNew);
+                binding.recycleProductNewHorizontal.setAdapter(adapterHomeNewProduct);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
 }
