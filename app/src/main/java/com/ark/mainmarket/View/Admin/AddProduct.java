dd4e808c50abc5427c81e00b782d646b5b1010c4
aditgocendra <gocendra123@gmail.com>
2022-02-28 15:25:55 +0800

    update new attr

--- app/src/main/java/com/ark/mainmarket/View/Admin/AddProduct.java
+++ app/src/main/java/com/ark/mainmarket/View/Admin/AddProduct.java
@@ -3,66 +3,57 @@ package com.ark.mainmarket.View.Admin;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.ContextMenu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.WindowManager;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
-import android.widget.CompoundButton;
 import android.widget.Toast;
-
+import com.ark.mainmarket.Model.ModelCategory;
 import com.ark.mainmarket.Model.ModelProduct;
 import com.ark.mainmarket.R;
 import com.ark.mainmarket.Utility;
 import com.ark.mainmarket.databinding.ActivityProductAddBinding;
-import com.ark.mainmarket.databinding.LayoutDialogPromoBinding;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.switchmaterial.SwitchMaterial;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
-
-import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
-
 import petrov.kristiyan.colorpicker.ColorPicker;
 
 public class AddProduct extends AppCompatActivity {
 
     private ActivityProductAddBinding binding;
 
+    // pick image gallery
     private final int PICK_IMAGE_GALLERY = 8888;
     private int onPickMedia = 0;
     private Uri fileUri;
 
+    // bottom dialog
     private BottomSheetDialog bottomSheetDialog;
+    private TextInputEditText priceWholesaleTiAdd, minBuyWholesale;
+    private SwitchMaterial switchFreeSending, switchWholeSaleProduct;
+    private AutoCompleteTextView autoCompleteDisc;
+    private String categorySelectKey = null;
 
-    private SwitchMaterial switchFreeSending;
-    private SwitchMaterial switchWholeSaleProduct;
-    private TextInputEditText priceWholesaleTiAdd;
-    private TextInputEditText discProduct;
-    private Button finishPromoBtn;
-
+    // database
+    private final DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,143 +62,147 @@ public class AddProduct extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         Utility.checkWindowSetFlag(this);
-//        binding.cardThumbsImage.setBackgroundResource(R.color.blue_product);
+        listenerClick();
 
-        binding.imageProductAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                pickImageOnGalery();
-            }
-        });
+    }
 
+    private void listenerClick() {
+        binding.backBtn.setOnClickListener(view -> finish());
 
-        binding.saveProduct.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (binding.nameItemTiAdd.getText().toString().isEmpty()){
-                    binding.nameItemTiAdd.setError("Tidak boleh kosong");
-                }else if (binding.priceItemNormalTiAdd.getText().toString().isEmpty()){
-                    binding.priceItemNormalTiAdd.setError("Tidak boleh kosong");
+        binding.imageProductAdd.setOnClickListener(view -> pickImageOnGalery());
+
+        binding.saveProduct.setOnClickListener(view -> {
+            String nameItem = binding.nameItemTiAdd.getText().toString();
+            String priceNormalItem = binding.priceItemNormalTiAdd.getText().toString();
+            String condition = binding.autoCompleteCondition.getText().toString();
+            String stock = binding.autoCompleteStock.getText().toString();
+            String desc = binding.descriptionItemTiAdd.getText().toString();
+
+            if (nameItem.isEmpty()){
+                binding.nameItemTiAdd.setError("Tidak boleh kosong");
+            }else if (priceNormalItem.isEmpty()){
+                binding.priceItemNormalTiAdd.setError("Tidak boleh kosong");
+            }else if (condition.isEmpty()){
+                binding.autoCompleteCondition.setError("Tidak boleh kosong");
+            }else if (stock.isEmpty()){
+                binding.autoCompleteStock.setError("Tidak boleh kosong");
+            }else if (desc.isEmpty()){
+                binding.descriptionItemTiAdd.setError("Tidak boleh kosong");
+            }else if (categorySelectKey == null){
+                Utility.toastLS(AddProduct.this, "Anda belum memilih kategori");
+            }else {
+                if (onPickMedia > 0){
+                    binding.saveProduct.setEnabled(false);
+                    binding.progressCircular.setVisibility(View.VISIBLE);
+                    saveImageProduct();
                 }else {
-                    if (onPickMedia > 0){
-                        binding.saveProduct.setEnabled(false);
-                        saveImageProduct();
-                    }else {
-                        Toast.makeText(AddProduct.this, "Anda belum mengupload photo", Toast.LENGTH_SHORT).show();
-                    }
+                    Toast.makeText(AddProduct.this, "Anda belum mengupload photo", Toast.LENGTH_SHORT).show();
                 }
             }
         });
 
         setItemAutoComplete();
 
-
         bottomSheetDialog = new BottomSheetDialog(AddProduct.this);
         setBottomDialog();
-        binding.promoSaleBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                bottomSheetDialog.show();
-
-            }
-        });
+        binding.promoSaleBtn.setOnClickListener(view -> bottomSheetDialog.show());
 
 //      color picker
-        binding.colorPicker.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ColorPicker colorPicker = new ColorPicker(AddProduct.this);
-                colorPicker.show();
-                colorPicker.setOnChooseColorListener(new ColorPicker.OnChooseColorListener() {
-                    @Override
-                    public void onChooseColor(int position,int color) {
-                        binding.cardThumbsImage.setCardBackgroundColor(color);
-                    }
+        binding.colorPicker.setOnClickListener(view -> {
+            ColorPicker colorPicker = new ColorPicker(AddProduct.this);
+            colorPicker.show();
+            colorPicker.setOnChooseColorListener(new ColorPicker.OnChooseColorListener() {
+                @Override
+                public void onChooseColor(int position,int color) {
+                    binding.cardThumbsImage.setCardBackgroundColor(color);
+                }
 
-                    @Override
-                    public void onCancel(){
-                        colorPicker.dismissDialog();
-                    }
-                });
-            }
+                @Override
+                public void onCancel(){
+                    colorPicker.dismissDialog();
+
+                }
+            });
         });
-
     }
 
     private void setBottomDialog(){
         View viewBottomDialog = getLayoutInflater().inflate(R.layout.layout_dialog_promo, null, false);
-        SwitchMaterial switchFreeSending = viewBottomDialog.findViewById(R.id.switch_free_sending);
-        SwitchMaterial switchWholeSaleProduct = viewBottomDialog.findViewById(R.id.switch_wholesale_product);
-        TextInputEditText priceWholesaleTiAdd = viewBottomDialog.findViewById(R.id.price_wholesale_ti_add);
-        TextInputEditText minBuyWholesale = viewBottomDialog.findViewById(R.id.min_buy_wholesale);
-        AutoCompleteTextView autoCompleteDisc = viewBottomDialog.findViewById(R.id.auto_complete_disc);
+        switchFreeSending = viewBottomDialog.findViewById(R.id.switch_free_sending);
+        switchWholeSaleProduct = viewBottomDialog.findViewById(R.id.switch_wholesale_product);
+        priceWholesaleTiAdd = viewBottomDialog.findViewById(R.id.price_wholesale_ti_add);
+        minBuyWholesale = viewBottomDialog.findViewById(R.id.min_buy_wholesale);
+        autoCompleteDisc = viewBottomDialog.findViewById(R.id.auto_complete_disc);
+
+        TextInputLayout layoutPriceWholesaleTiAdd = viewBottomDialog.findViewById(R.id.layout_price_wholesale_ti_add);
+        TextInputLayout layoutMinBuyWholesale = viewBottomDialog.findViewById(R.id.layout_min_buy_wholesale);
         Button finishPromoBtn = viewBottomDialog.findViewById(R.id.finish_promo_btn);
 
-        String[] disc = {"10", "20", "50", "70", "90"};
+        String[] disc = {"-","10", "20", "50", "70", "90"};
         ArrayAdapter<String> discAdapter;
         discAdapter = new ArrayAdapter<>(this, R.layout.layout_option_item, disc);
         autoCompleteDisc.setAdapter(discAdapter);
-        autoCompleteDisc.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String item = adapterView.getItemAtPosition(i).toString();
+
+        switchWholeSaleProduct.setOnCheckedChangeListener((compoundButton, checked) -> {
+            if (checked){
+                layoutPriceWholesaleTiAdd.setVisibility(View.VISIBLE);
+                layoutMinBuyWholesale.setVisibility(View.VISIBLE);
+            }else {
+                layoutPriceWholesaleTiAdd.setVisibility(View.GONE);
+                layoutMinBuyWholesale.setVisibility(View.GONE);
             }
         });
 
-        switchWholeSaleProduct.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
-                if (checked){
-                    priceWholesaleTiAdd.setEnabled(true);
-                }else {
-                    priceWholesaleTiAdd.setEnabled(false);
-                }
-            }
-        });
-
-        finishPromoBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                bottomSheetDialog.dismiss();
-            }
-        });
+        finishPromoBtn.setOnClickListener(view -> bottomSheetDialog.dismiss());
 
         bottomSheetDialog.setContentView(viewBottomDialog);
     }
 
     private void setItemAutoComplete(){
-
         String[] condition = {"Baru", "Bekas"};
         ArrayAdapter<String> conditionAdapter;
         conditionAdapter = new ArrayAdapter<>(this, R.layout.layout_option_item, condition);
         binding.autoCompleteCondition.setAdapter(conditionAdapter);
-        binding.autoCompleteCondition.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String item = adapterView.getItemAtPosition(i).toString();
-            }
-        });
 
         String[] stock = {"> 10", "> 100", "> 1000"};
         ArrayAdapter<String> stockAdapter;
         stockAdapter = new ArrayAdapter<>(this, R.layout.layout_option_item, stock);
         binding.autoCompleteStock.setAdapter(stockAdapter);
-        binding.autoCompleteStock.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String item = adapterView.getItemAtPosition(i).toString();
-            }
-        });
+
+        setCategoryProduct();
 
     }
 
+    private void setCategoryProduct() {
+        ArrayAdapter<ModelCategory> categoryAdapter = new ArrayAdapter<>(this, R.layout.layout_option_item);
+        reference.child("category").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot ds : snapshot.getChildren()){
+                    ModelCategory modelCategory = ds.getValue(ModelCategory.class);
+                    modelCategory.setKey(ds.getKey());
+                    categoryAdapter.add(modelCategory);
+                }
+                binding.autoCompleteCategory.setAdapter(categoryAdapter);
+            }
 
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Utility.toastLS(AddProduct.this, "Category Database"+error.getMessage());
+            }
+        });
+
+        binding.autoCompleteCategory.setOnItemClickListener((adapterView, view, i, l) -> {
+            ModelCategory modelCategory = (ModelCategory) adapterView.getItemAtPosition(i);
+            categorySelectKey = modelCategory.getKey();
+        });
+    }
 
     private void pickImageOnGalery() {
         Intent intent = new Intent();
         intent.setType("image/*");
         intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(intent, 8888);
+        startActivityForResult(intent, PICK_IMAGE_GALLERY);
     }
 
     @Override
@@ -229,7 +224,6 @@ public class AddProduct extends AppCompatActivity {
                 }
             }
         }
-
     }
 
     private void saveImageProduct(){
@@ -238,60 +232,59 @@ public class AddProduct extends AppCompatActivity {
         String fileName = dateFormat.format(now);
 
         StorageReference storageRef = FirebaseStorage.getInstance().getReference("product_image/"+fileName);
+        UploadTask uploadTask = storageRef.putFile(fileUri);
 
-        Bitmap bitmap = ((BitmapDrawable) binding.imageProductAdd.getDrawable()).getBitmap();
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 40, baos);
-        byte[] data = baos.toByteArray();
-
-        UploadTask uploadTask = storageRef.putBytes(data);
-
-        uploadTask.addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(AddProduct.this, "Photo gagal diupload", Toast.LENGTH_SHORT).show();
-                binding.saveProduct.setEnabled(true);
-            }
-        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-            @Override
-            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                storageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                    @Override
-                    public void onSuccess(Uri uri) {
-//                        saveProduct(String.valueOf(uri));
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(AddProduct.this, "Url photo gagal didownload", Toast.LENGTH_SHORT).show();
-                        binding.saveProduct.setEnabled(true);
-                    }
-                });
-            }
-        });
+        uploadTask.addOnFailureListener(e -> {
+            Toast.makeText(AddProduct.this, "Photo gagal diupload", Toast.LENGTH_SHORT).show();
+            binding.progressCircular.setVisibility(View.GONE);
+            binding.saveProduct.setEnabled(true);
+        }).addOnSuccessListener(taskSnapshot -> storageRef.getDownloadUrl().addOnSuccessListener(uri ->
+                saveProduct(String.valueOf(uri))).addOnFailureListener(e -> {
+            Toast.makeText(AddProduct.this, "Url photo gagal didownload", Toast.LENGTH_SHORT).show();
+            binding.progressCircular.setVisibility(View.GONE);
+            binding.saveProduct.setEnabled(true);
+        }));
     }
 
-//    private void saveProduct(String url){
-//        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
-//
-//        ModelProduct modelProduct = new ModelProduct(
-//          binding.nameItemTiAdd.getText().toString(),
-//          binding.priceItemNormalTiAdd.getText().toString(),
-//          binding.descriptionItemTiAdd.getText().toString(),
-//          url
-//        );
-//
-//        reference.child("product").push().setValue(modelProduct).addOnCompleteListener(new OnCompleteListener<Void>() {
-//            @Override
-//            public void onComplete(@NonNull Task<Void> task) {
-//                if (task.isSuccessful()){
-//                    Toast.makeText(AddProduct.this, "Berhasil menambahkan data", Toast.LENGTH_SHORT).show();
-//                    binding.saveProduct.setEnabled(true);
-//                }else {
-//                    Toast.makeText(AddProduct.this, "Gagal menambahkan data", Toast.LENGTH_SHORT).show();
-//                    binding.saveProduct.setEnabled(true);
-//                }
-//            }
-//        });
-//    }
+    private void saveProduct(String url){
+        String nameItem = binding.nameItemTiAdd.getText().toString();
+        String priceNormalItem = binding.priceItemNormalTiAdd.getText().toString();
+        String condition = binding.autoCompleteCondition.getText().toString();
+        String stock = binding.autoCompleteStock.getText().toString();
+        String desc = binding.descriptionItemTiAdd.getText().toString();
+        boolean freeSending = false, wholeSale = false;
+        String priceWholeSale = "-", minWholeSale= "-";
+        String disc = autoCompleteDisc.getText().toString();
+        String backgroundColorProduct = String.valueOf(binding.cardThumbsImage.getCardBackgroundColor().getDefaultColor());
+
+        if (switchFreeSending.isChecked()){
+            freeSending = true;
+        }
+
+        if (switchWholeSaleProduct.isChecked()){
+            wholeSale = true;
+            priceWholeSale = priceWholesaleTiAdd.getText().toString();
+            minWholeSale = minBuyWholesale.getText().toString();
+        }
+
+        ModelProduct modelProduct = new ModelProduct(
+          nameItem,
+          priceNormalItem,
+          categorySelectKey,
+          condition,
+          stock, desc, url, backgroundColorProduct, freeSending, wholeSale, priceWholeSale, minWholeSale, disc
+
+        );
+
+        reference.child("product").push().setValue(modelProduct).addOnCompleteListener(task -> {
+            if (task.isSuccessful()){
+                Toast.makeText(AddProduct.this, "Berhasil menambahkan data", Toast.LENGTH_SHORT).show();
+                finish();
+            }else {
+                Toast.makeText(AddProduct.this, "Gagal menambahkan data", Toast.LENGTH_SHORT).show();
+            }
+            binding.progressCircular.setVisibility(View.GONE);
+            binding.saveProduct.setEnabled(true);
+        });
+    }
 }
