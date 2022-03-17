0d7b2859e2b3f3fdb6364f7499ec9d13e2476a36
aditgocendra <gocendra123@gmail.com>
2022-03-17 22:16:51 +0800

    add global check gps

--- app/src/main/java/com/ark/mainmarket/Utility.java
+++ app/src/main/java/com/ark/mainmarket/Utility.java
@@ -3,7 +3,9 @@ package com.ark.mainmarket;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.graphics.Color;
+import android.location.LocationManager;
 import android.os.Build;
 import android.util.Log;
 import android.view.View;
@@ -13,6 +15,13 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -82,6 +91,44 @@ public class Utility {
         NumberFormat rupiahFormat = NumberFormat.getNumberInstance(localeID);
         return "Rp. "+rupiahFormat.format(decimalNumber);
     }
+
+    public static boolean isGPSEnabled(Context mContext){
+        LocationManager locationManager = (LocationManager) mContext.getSystemService(Context.LOCATION_SERVICE);
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+    }
+
+    public static void turnOnGPS(LocationRequest locationRequest, Activity activity) {
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(locationRequest);
+        builder.setAlwaysShow(true);
+        Task<LocationSettingsResponse> responseTask = LocationServices.getSettingsClient(activity).checkLocationSettings(builder.build());
+
+        responseTask.addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                try {
+                    task.getResult(ApiException.class);
+                    Toast.makeText(activity, "GPS IS ON", Toast.LENGTH_SHORT).show();
+                } catch (ApiException e) {
+                    switch (e.getStatusCode()) {
+                        case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                            try {
+                                ResolvableApiException resolvableApiException = (ResolvableApiException) e;
+                                resolvableApiException.startResolutionForResult(activity, 1005);
+                            } catch (IntentSender.SendIntentException ex) {
+                                ex.printStackTrace();
+                            }
+                            break;
+                        case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                            Toast.makeText(activity, "Device do not have location", Toast.LENGTH_SHORT).show();
+                            break;
+                    }
+                }
+            } else {
+
+                Toast.makeText(activity, "Looks like GPS is off", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
     
     
 }
