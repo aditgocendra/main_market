c4418891690a7553b9ee53011e5f05129a7e9ff4
gocen <gocendra123@gmail.com>
2022-03-22 11:00:10 +0700

    set data favorite user

--- app/src/main/java/com/ark/mainmarket/View/User/Fragment/Favorite.java
+++ app/src/main/java/com/ark/mainmarket/View/User/Fragment/Favorite.java
@@ -1,66 +1,133 @@
 package com.ark.mainmarket.View.User.Fragment;
 
 import android.os.Bundle;
-
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+import com.ark.mainmarket.Adapter.AdapterFavorite;
+import com.ark.mainmarket.Utility;
+import com.ark.mainmarket.databinding.FragmentFavoriteBinding;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
-import com.ark.mainmarket.R;
-
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Favorite#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class Favorite extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private List<String> listFavorite;
+    private AdapterFavorite adapterFavorite;
+    private final DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public Favorite() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Favorite.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Favorite newInstance(String param1, String param2) {
-        Favorite fragment = new Favorite();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private long countData;
+    private String key = null;
+    private boolean isLoadData = false;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        reference.child("product_favorite").child(Utility.uidCurrentUser).get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()){
+                countData = task.getResult().getChildrenCount();
+                isLoadData = true;
+                setDataFavUser();
+            }else {
+                Toast.makeText(requireContext(), Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
+    private FragmentFavoriteBinding binding;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite, container, false);
+        binding = FragmentFavoriteBinding.inflate(inflater, container, false);
+
+        RecyclerView.LayoutManager layoutManagerCategory = new LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false);
+        binding.recycleFavorite.setHasFixedSize(true);
+        binding.recycleFavorite.setLayoutManager(layoutManagerCategory);
+
+        adapterFavorite = new AdapterFavorite(requireContext());
+        binding.recycleFavorite.setAdapter(adapterFavorite);
+
+        binding.recycleFavorite.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+
+                // get total item in list category
+                LinearLayoutManager linearLayoutManager = (LinearLayoutManager) binding.recycleFavorite.getLayoutManager();
+                assert linearLayoutManager != null;
+                int totalItem = linearLayoutManager.getItemCount();
+                Log.d("Total Item", String.valueOf(totalItem));
+                // check scroll on bottom
+                if (!binding.recycleFavorite.canScrollVertically(1) && newState==RecyclerView.SCROLL_STATE_IDLE){
+                    // check data item if total item < total data in database == load more data
+                    if (totalItem < countData){
+                        // load more data
+                        if (!isLoadData){
+                            isLoadData = true;
+                            setDataFavUser();
+                        }
+                    }
+                }
+            }
+        });
+
+        return binding.getRoot();
+    }
+
+    private void setDataFavUser(){
+        if (!isLoadData){
+            return;
+        }
+
+        Query query;
+        if (key == null){
+            query = reference.child("product_favorite").child(Utility.uidCurrentUser).orderByKey().limitToFirst(10);
+        }else {
+            query = reference.child("product_favorite").child(Utility.uidCurrentUser).orderByKey().startAfter(key).limitToFirst(10);
+        }
+
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                listFavorite = new ArrayList<>();
+                for (DataSnapshot ds : snapshot.getChildren()){
+                    String productKeyFav = ds.getKey();
+                    if (productKeyFav != null){
+                        key = ds.getKey();
+                        listFavorite.add(productKeyFav);
+                    }
+                }
+                Handler handler = new Handler();
+                handler.postDelayed(() -> {
+                    if (listFavorite.size() != 0){
+                        adapterFavorite.setItem(listFavorite);
+                        adapterFavorite.notifyDataSetChanged();
+                    }
+                    isLoadData = false;
+
+                },100);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(requireContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
